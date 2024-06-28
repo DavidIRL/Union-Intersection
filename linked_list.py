8e63429b033fa1b29a1627e5c03b2a2e80395b20
DavidIRL <david.white0911@gmail.com>
2024-06-28 18:53:02 -0400

    update linked_list.py w/ Node and LinkedList classes

---
+++ linked_list.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_elements = 0
+
+
+    def __str__(self):
+        current = self.head
+        output = []
+
+        while current:
+            output.append(current.value)
+            current = current.next
+        return "->".join(output)
+
+
+    def append(self, value):
+        if value is None:
+            raise ValueError('Node value cannot be empty')
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            self.num_elements = 1
+        
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
+        self.num_elements += 1
+        
+
+    def size(self):
+        return self.num_elements
+
+
+
+        
+
+
+
+
+
+
+
+
